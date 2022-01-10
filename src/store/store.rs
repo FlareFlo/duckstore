338fd282a5f310cf608e5b13c179aeb3b5990184
FlareFlo <florianlipp17@gmail.com>
2022-01-10 14:12:51 +0100

    Using raw bytes instead of serde
    Took 14 minutes

--- src/store/store.rs
+++ src/store/store.rs
@@ -1,49 +1,37 @@
 use std::fs;
+
 use directories::ProjectDirs;
 
-pub trait Store<'a>: serde::Serialize + serde::Deserialize<'a> {
-	fn store_local_data(&self, project_name: &str) -> Result<(), String> {
-		if let Some(dirs) = ProjectDirs::from("", "", project_name) {
-			if let Ok(json) = serde_json::to_string(self) {
-				if let Some(string_dir) = dirs.data_dir().to_str() {
-					if let Err(error) = fs::write(string_dir, json) {
-						Err(format!("{}", error))
-					} else {
-						Ok(())
-					}
-				} else {
-					Err(format!("Failed to resolve path into string"))
-				}
+pub trait Store: std::io::Read {
+	fn store_local_data(data: &[u8], project_name: &str) -> Result<(), String> where Self: Sized, Self: AsRef<[u8]> {
+		if let Some(dir_string) = resolve_path(project_name) {
+			if let Err(error) = fs::write(dir_string, data) {
+				Err(format!("{}", error))
 			} else {
-				Err(format!("Failed to resolve path {} to string", project_name))
+				Ok(())
 			}
 		} else {
-			Err(format!("Failed to resolve {} OS path", project_name))
+			Err(format!("Cannot resolve {} to path", project_name))
 		}
 	}
-	fn load_local_data(project_name: &str) -> Result<Self, String> {
-
-				if let Ok(from_reader) = fs::read(dir_string) {
-					return if let Ok(parsed) = serde_json::from_slice(&from_reader) {
-						Ok(parsed)
-					} else {
-						Err(format!("Failed to parse struct"))
-					};
-				} else {
-					Err(format!("Failed to read file"))
-				}
-
+	fn load_local_data(project_name: &str) -> Result<Vec<u8>, String> where Self: Sized {
+		if let Some(dir_string) = resolve_path(project_name) {
+			if let Ok(from_reader) = fs::read(dir_string) {
+				return Ok(from_reader);
+			} else {
+				Err(format!("Failed to read file"))
+			}
+		} else {
+			Err(format!("Cannot resolve {} to path", project_name))
+		}
 	}
 }
 
 fn resolve_path(project_name: &str) -> Option<String> {
 	if let Some(dirs) = ProjectDirs::from("", "", project_name) {
 		if let Some(dir_string) = dirs.data_dir().to_str() {
-			Some(dir_string.to_string())
-		} else {
-			Err(format!("Failed to resolve path {} to string", project_name))
+			return Some(dir_string.to_string());
 		}
-	} else {
-		Err(format!("Failed to resolve {} to OS path", project_name))
 	}
+	None
 }
